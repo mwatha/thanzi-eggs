298d423030cb74f8fe97e13fade0f0f178f7f02f
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-02-23 15:38:04 +0200

    first co

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThanziEggs::Application.config.secret_token = '2e1d6d345bd71c09e597a10cd97ec59f1c2196993b7d58984b85791270d89f0212a57f53be46cb43c6a2cbb29b4ff9d623c88fff728f762206dfae3ec528dc66'
